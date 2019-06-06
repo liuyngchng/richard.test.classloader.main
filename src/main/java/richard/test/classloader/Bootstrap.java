52733dd4a0547e00b233400af9872334db3829e6
Richard <richard@RicharddeMac-mini.local>
2019-06-06 08:06:39 +0800

    debug

---
+++ src/main/java/richard/test/classloader/Bootstrap.java
@@ -0,0 +1,8 @@
+package richard.test.classloader;
+
+public class Bootstrap {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+        testClass.sayHello();
+    }
+}
